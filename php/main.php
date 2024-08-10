5ce7777def438efbf453571900ce31f8d051a462
Andrey Garcia M <andrey56estiven@gmail.com>
2024-08-10 17:35:15 -0500

    Comandos php

---
+++ php/main.php
@@ -0,0 +1,54 @@
+<?php
+#Sintaxis de php
+#Tipos de variables
+
+#Enteros
+$entero = 10;
+#Flotantes 
+$Decimal = 10.5;
+#Cadenas
+$cadena = "Hola mundo"
+#Booleanos
+$verdadero = true;
+$falso = false;
+#Ciclos
+# ciclo for 
+for ($i=0; $i < ; $i++) { 
+    # code...
+}
+#Cilcos
+# ciclos while
+$i = 0
+while ($i <= 10) {
+    echo $i . "<br>"; #Para concatenar utilizo punto
+    $i++;
+}
+#Ciclo while
+$i = 0;
+do{
+    print $i . "<br>";
+    $i ++;
+}while ($i < 10);
+#Estuctura de control 
+if ($Verdadero)
+    if ($Falso){
+        echo "Verdadero";
+    }else{
+        echo"Falso";
+}else{
+    echo"Falso";
+}
+#Funciones
+function suma($a, $b){
+    return $a + $b;
+}
+#Arrays
+$array = array(1,2,3,4,5,6,7,8,8,10);
+#Arrays de texto
+$array = array("uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez");
+
+
+    
+
+
+?>
